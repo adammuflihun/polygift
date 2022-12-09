284c77f3416e953cc008ce88371e2f7ee2050b96
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-09 09:42:41 +0700

    s

--- src/utils/gsapHome.ts
+++ src/utils/gsapHome.ts
@@ -3,29 +3,54 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import SplitType from 'split-type';
 
 export const gsapHome = () => {
-  // gsap.registerPlugin(ScrollTrigger);
-
+  gsap.registerPlugin(ScrollTrigger);
+  const tl = gsap.timeline();
+  //title text 1
   let addAnimation = function () {
-    $('.title-unique').each(function (index) {
+    $('.title-unique:nth-child(1)').each(function (index) {
       const text = new SplitType($(this), {
         types: 'lines, words, chars',
         lineClass: 'word-line',
       });
-
-      // let textInstance = $(this);
-      // let line = textInstance.find('.word-line');
-      // let word = $(this).find('.word');
       const chars = $(this).find('.char');
-      gsap.from(chars, {
-        y: '2rem',
+      tl.from(chars, 2.5, {
+        y: '8rem',
         opacity: 0,
         stagger: {
-          each: 0.3,
+          each: 0.1,
         },
         ease: 'Expo.easeOut',
       });
     });
   };
+  //title text 1
+
+  //title text 2
+  let addAnimation2 = function () {
+    $('.title-unique:nth-child(2)').each(function (index) {
+      const text = new SplitType($(this), {
+        types: 'lines, words, chars',
+        lineClass: 'word-line',
+      });
+      const chars = $(this).find('.char');
+      tl.from(
+        chars,
+        2,
+        {
+          y: '8rem',
+          opacity: 0,
+          stagger: {
+            each: 0.08,
+            from: 'start',
+          },
+          ease: 'Expo.easeOut',
+        },
+        0.5
+      );
+    });
+  };
+  //title text 2
 
   addAnimation();
+  addAnimation2();
 };
