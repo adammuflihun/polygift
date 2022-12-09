ccba7c7dd27a57fcf0629d007276a96fbf69ee4f
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-09 10:03:58 +0700

    s

--- src/utils/gsapHome.ts
+++ src/utils/gsapHome.ts
@@ -51,6 +51,88 @@ export const gsapHome = () => {
   };
   //title text 2
 
+  //calling function
   addAnimation();
   addAnimation2();
+  //calling function
+
+  // Continue timeline navbar
+  tl.from(
+    '.navbar',
+    1,
+    {
+      y: '-4rem',
+      opacity: 0,
+      stagger: {
+        each: 0.08,
+        from: 'start',
+      },
+      ease: 'Expo.easeOut',
+    },
+    1.2
+  );
+  // Continue timeline navbar
+
+  // supported-text-headline
+  tl.from(
+    '.supported-text-headline',
+    1,
+    {
+      y: '4rem',
+      opacity: 0,
+      stagger: {
+        each: 0.08,
+        from: 'start',
+      },
+      ease: 'Expo.easeOut',
+    },
+    0.9
+  );
+  // supported-text-headline
+
+  // wrap gallery
+  gsap.from('.item-img-gallery', 2.5, {
+    scrollTrigger: {
+      trigger: '.wrap-img-gallery',
+      start: 'top 100%',
+      end: '0% 100%',
+      // markers: true,
+      toggleActions: 'play none reverse none',
+    },
+    y: '4rem',
+    opacity: 0,
+    stagger: {
+      each: 0.3,
+    },
+    ease: 'Expo.easeOut',
+  });
+  // wrap gallery
+
+  let addAnimation3 = function () {
+    $('.title-unique2').each(function (index) {
+      const text = new SplitType($(this), {
+        types: 'lines, words, chars',
+        lineClass: 'word-line',
+      });
+      const words = $(this).find('.word');
+      gsap.from(words, 1.5, {
+        scrollTrigger: {
+          trigger: this,
+          start: 'top 100%',
+          end: '0% 100%',
+          // markers: true,
+          toggleActions: 'play none none none',
+        },
+        y: '8rem',
+        opacity: 0,
+        stagger: {
+          each: 0.05,
+        },
+        ease: 'Expo.easeOut',
+      });
+    });
+  };
+  addAnimation3();
+
+  // wrap gallery
 };
