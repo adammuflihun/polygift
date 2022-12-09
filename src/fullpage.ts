1a25b4ea90735c4162e0e24836a0c9c7f61f5584
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-10 04:25:49 +0700

    s

---
+++ src/fullpage.ts
@@ -0,0 +1,16 @@
+import Styles from 'src/styles/globalStyles';
+
+import { fullPage } from '$utils/fullPage';
+import { gsapHome } from '$utils/gsapHome';
+import { scrollTo } from '$utils/scrollTo';
+
+window.Webflow ||= [];
+window.Webflow.push(() => {
+  gsapHome();
+  fullPage();
+  // scrollTo();
+});
+
+$(document).ready(function () {
+  $('head').append("<link rel='stylesheet' href='http://localhost:3000/index.css'>");
+});
