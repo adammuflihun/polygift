54765afe66940a057141be362566db5834cb2f4c
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-09 09:34:28 +0700

    s

--- src/utils/gsapHome.ts
+++ src/utils/gsapHome.ts
@@ -3,30 +3,26 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import SplitType from 'split-type';
 
 export const gsapHome = () => {
-  gsap.registerPlugin(ScrollTrigger);
+  // gsap.registerPlugin(ScrollTrigger);
 
   let addAnimation = function () {
     $('.title-unique').each(function (index) {
       const text = new SplitType($(this), {
-        types: 'lines, words',
+        types: 'lines, words, chars',
         lineClass: 'word-line',
       });
 
-      let textInstance = $(this);
-      let line = textInstance.find('.word-line');
-      let word = $(this).find('.word');
-
-      gsap.from(word, {
-        // scrollTrigger: {
-        //   trigger: '.title-unique',
-        //   start: 'top 1rem',
-        //   end: 'top 1rem',
-        //   toggleActions: 'play none reverse none',
-        // },
+      // let textInstance = $(this);
+      // let line = textInstance.find('.word-line');
+      // let word = $(this).find('.word');
+      const chars = $(this).find('.char');
+      gsap.from(chars, {
         y: '2rem',
         opacity: 0,
-        duration: 0.05,
-        ease: 'expo.out',
+        stagger: {
+          each: 0.3,
+        },
+        ease: 'Expo.easeOut',
       });
     });
   };
