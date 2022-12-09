66671b051dc9d51dbb0533ce5a7e638a5a644409
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-10 06:02:25 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Styles from 'src/styles/globalStyles';
+import Styles from 'src/styles/styleSnap';
+
 
 import { fullPage } from '$utils/fullPage';
 import { gsapHome } from '$utils/gsapHome';
@@ -7,7 +9,7 @@ import { scrollTo } from '$utils/scrollTo';
 window.Webflow ||= [];
 window.Webflow.push(() => {
   gsapHome();
-  fullPage();
+  // fullPage();
   // scrollTo();
 });
 
