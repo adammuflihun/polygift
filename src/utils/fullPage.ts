53254df771304544a902750ce87f9a8acbb92cfc
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-10 09:08:15 +0700

    s

--- src/utils/fullPage.ts
+++ src/utils/fullPage.ts
@@ -3,10 +3,14 @@ import SplitType from 'split-type';
 
 export const fullPage = () => {
   new fullpage('#fullpage', {
-    licenseKey: 'YOUR_KEY_HERE',
+    css3: false,
+    scrollingSpeed: 700,
+    // scrollBar: true,
+    // lockAnchors: false,
+    licenseKey: '6A0A57E3-ADC34D84-A91F6D3E-392746EE',
     autoScrolling: true,
     scrollHorizontally: true,
-    credits: { enabled: false, label: '', position: 'right' },
+    // credits: { enabled: false, label: '', position: 'right' },
   });
   console.log('fullpage');
 };
--- src/utils/gsapObserver.ts
+++ src/utils/gsapObserver.ts
@@ -1,6 +1,46 @@
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { Observer } from 'gsap/Observer';
 
 export const gsapObserver = () => {
   console.log('gsapobserver');
+
+  gsap.registerPlugin(Observer);
+  const tl = gsap.timeline();
+  const tl2 = gsap.timeline();
+
+  $('#slide1number2').click(function () {
+    // code to be executed when the button is clicked
+    tl.fromTo(
+      '#slide1number2title',
+      { opacity: 0, autoAlpha: 0, y: '2rem' },
+      { opacity: 1, autoAlpha: 1, y: '0rem', ease: 'Expo.easeOut' }
+    );
+    tl.fromTo(
+      '#slide1number2subtitle',
+      { opacity: 0, autoAlpha: 0 },
+      { opacity: 1, autoAlpha: 1, ease: 'Expo.easeOut' },
+      0.3
+    );
+    gsap.to('#slide1number2titledrag', { duration: 1, x: '2.3rem', ease: 'Expo.easeOut' });
+    tl.to('#slide1number2', { color: 'white' }, 0.2);
+    tl.to('#slide1number2titlenumber1', { color: '#000000' }, 0.2);
+  });
+
+  $('#slide1number2titlenumber1').click(function () {
+    // code to be executed when the button is clicked
+    tl2.fromTo(
+      '#slide1number2title',
+      { opacity: 1, autoAlpha: 1, y: '2rem' },
+      { opacity: 0, autoAlpha: 0, y: '0', ease: 'Expo.easeOut' }
+    );
+    tl2.fromTo(
+      '#slide1number2subtitle',
+      { opacity: 1, autoAlpha: 1 },
+      { opacity: 0, autoAlpha: 0, ease: 'Expo.easeOut' },
+      0.2
+    );
+    gsap.to('#slide1number2titledrag', { duration: 1, x: '0rem', ease: 'Expo.easeOut' });
+    tl2.to('#slide1number2', { color: '#000000' }, 0.2);
+    tl2.to('#slide1number2titlenumber1', { color: 'white' }, 0.2);
+  });
 };
